3b456c7fc97ff94995f3dfb7dce335ef243cda29
Sanskar Modi <modisanskar5@gmail.com>
2023-06-04 22:27:14 +0530

    List of Users Updated on Disconnect

--- src/index.ts
+++ src/index.ts
@@ -6,18 +6,49 @@ import { Server } from "socket.io";
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
-var users: any = [];
+
+var clients: any = [];
+let incr = 1;
+
+const getUsersList = () => {
+  var usersList: any = [];
+  for (var i = 0; i < clients.length; i++) {
+    usersList[i] = clients[i].n;
+  }
+  return usersList;
+};
 io.on("connection", (socket) => {
-  socket.on("chat message", (msg) => {
+  clients.push(socket);
+  socket.on("send chat message", (msg) => {
     io.emit("chat message", msg);
   });
 
-  socket.on("envia nick", (socket) => {
-    io.emit("info", "New user: " + socket);
-    console.log(socket);
-    users.push(socket);
-    io.emit("users list", users);
-    console.log(users);
+  socket.on("start", () => {
+    console.info("Started.....");
+    io.emit("nick", "guest" + incr);
+    clients[clients.indexOf(socket)].n = "guest" + incr;
+    incr++;
+    io.emit("users list", getUsersList());
+  });
+
+  socket.on("set nick", function (nick) {
+    io.emit("info", "New user: " + nick); //console.log(nick);
+    clients[clients.indexOf(socket)].n = nick; //console.log(clients[clients.indexOf(socket)].n);
+    io.emit("users list", getUsersList()); //console.log(getUsersList());
+  });
+
+  socket.on("disconnect", function () {
+    if (clients[clients.indexOf(socket)].n == null) {
+      //console.log('Guest disconnect!');
+    } else {
+      //console.log(clients[clients.indexOf(socket)].n +' disconnect!');
+      io.emit(
+        "info",
+        "User " + clients[clients.indexOf(socket)].n + " disconnected."
+      );
+    }
+    clients.splice(clients.indexOf(socket), 1); //clientIndex, 1);
+    io.emit("users list", getUsersList());
   });
 });
 
