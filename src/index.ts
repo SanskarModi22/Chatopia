dae1a4dc99c141adae6a28e8b62aa29ee27e48cf
Sanskar Modi <modisanskar5@gmail.com>
2023-06-04 18:19:02 +0530

    Disconnect Event Added

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,26 @@
 import express, { Request, Response } from "express";
 import http from "http";
-import path from 'path';
+import path from "path";
 import { Server } from "socket.io";
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
 });
 
 const currentDirectory = __dirname;
-const parentDirectory = path.resolve(currentDirectory, '..');
+const parentDirectory = path.resolve(currentDirectory, "..");
 
-app.get('/', (req: Request, res: Response) => {
-  res.sendFile(parentDirectory + '/index.html');
+app.get("/", (req: Request, res: Response) => {
+  res.sendFile(parentDirectory + "/index.html");
 });
 
 server.listen(3000, () => {
-  console.log('listening on *:3000');
+  console.log("listening on *:3000");
 });
